ecb10bf2a73191b9a3bad60bd9d45d609b9d4b91
Muhammad <muhammadfajarisa@gmail.com>
2022-01-21 16:25:40 +0700

    Add project files.

---
+++ ServerKos-Config/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceKos;
+
+namespace ServerKos_Config
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Service1));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
